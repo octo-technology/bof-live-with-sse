ef00ab522aa1b7d2370083726f4c637dd5245814
Clotilde DESQUILBET <cdesquilbet@octo.com>
2018-05-02 15:18:31 +0200

    Add live score with differential sse (Back-End)

--- src/main/java/com/example/demo/infrastructure/controllers/streamers/LiveScoreStreamer.java
+++ src/main/java/com/example/demo/infrastructure/controllers/streamers/LiveScoreStreamer.java
@@ -2,6 +2,8 @@ package com.example.demo.infrastructure.controllers.streamers;
 
 import com.example.demo.domain.LiveScore;
 import com.example.demo.use_cases.GetLiveScore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -15,6 +17,7 @@ public class LiveScoreStreamer {
 
     private static final int INTERVAL_PERIOD_IN_MILLIS = 1_000;
     private static final String LIVE_SCORE_EVENT_NAME = "live-score";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private final GetLiveScore getLiveScore;
     private final Clock clock;
@@ -28,16 +31,29 @@ public class LiveScoreStreamer {
         AtomicReference<LiveScore> lastRecordedValue = new AtomicReference<>(null);
         return Flux.interval(Duration.ofMillis(INTERVAL_PERIOD_IN_MILLIS))
                 .map(tick -> {
-                    LiveScore newValue = getLiveScore.execute();
+                    try {
+                        LiveScore newValue = getLiveScore.execute();
+                        String newValueAsJson = OBJECT_MAPPER.writeValueAsString(newValue);
+                        String lastRecordedValueAsJson = OBJECT_MAPPER.writeValueAsString(lastRecordedValue.get());
 
-                    lastRecordedValue.set(newValue);
-                    return liveEvent(newValue);
+                        if (newValueAsJson.equals(lastRecordedValueAsJson)) {
+                            return keepAliveEvent();
+                        } else {
+                            lastRecordedValue.set(newValue);
+                            return liveEvent(newValue);
+                        }
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
+    }
+
+    private ServerSentEvent<LiveScoreEvent> keepAliveEvent() {
+        return ServerSentEvent.<LiveScoreEvent>builder().comment("keep-alive").event(LIVE_SCORE_EVENT_NAME).build();
     }
 
     private ServerSentEvent<LiveScoreEvent> liveEvent(LiveScore data) {
         LiveScoreEvent liveScoreEvent = new LiveScoreEvent(data, clock.millis());
         return ServerSentEvent.<LiveScoreEvent>builder().data(liveScoreEvent).event(LIVE_SCORE_EVENT_NAME).build();
     }
-
 }
--- src/test/java/com/example/demo/infrastructure/controllers/streamers/LiveScoreStreamerUTest.java
+++ src/test/java/com/example/demo/infrastructure/controllers/streamers/LiveScoreStreamerUTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.test.util.ReflectionTestUtils;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 import reactor.test.scheduler.VirtualTimeScheduler;
@@ -19,8 +20,8 @@ import java.time.Duration;
 import static java.time.LocalDateTime.of;
 import static java.time.ZoneOffset.UTC;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.objenesis.ObjenesisHelper.newInstance;
 
 @ExtendWith(MockitoExtension.class)
 class LiveScoreStreamerUTest {
@@ -38,6 +39,7 @@ class LiveScoreStreamerUTest {
     void setUp() {
         Clock fixedClock = Clock.fixed(of(2017, 9, 27, 11, 25, 12).toInstant(UTC), UTC);
         liveScoreStreamer = new LiveScoreStreamer(getLiveScore, fixedClock);
+
         virtualTimeScheduler = VirtualTimeScheduler.getOrSet();
     }
 
@@ -47,7 +49,7 @@ class LiveScoreStreamerUTest {
         @Test
         void return_no_event_during_the_first_three_seconds_period() {
             // given
-            LiveScore liveScore = mock(LiveScore.class);
+            LiveScore liveScore = newInstance(LiveScore.class);
             when(getLiveScore.execute()).thenReturn(liveScore);
 
             // when
@@ -63,7 +65,7 @@ class LiveScoreStreamerUTest {
         @Test
         void return_a_flux_of_server_sent_events_containing_live_from_repository_as_data_after_a_three_seconds_period() {
             // given
-            LiveScore liveScore = mock(LiveScore.class);
+            LiveScore liveScore = newInstance(LiveScore.class);
             when(getLiveScore.execute()).thenReturn(liveScore);
 
             // when
@@ -77,11 +79,96 @@ class LiveScoreStreamerUTest {
                     .thenCancel().verify();
         }
 
+        @Test
+        void return_two_distinct_times_when_live_has_changed_and_waiting_two_periods_of_three_seconds() {
+            // given
+            LiveScore liveScore = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(liveScore, "score", 1);
+            LiveScore anotherLiveScore = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(anotherLiveScore, "score", 3);
+            when(getLiveScore.execute()).thenReturn(liveScore).thenReturn(anotherLiveScore);
+
+            // when
+            Flux<ServerSentEvent<LiveScoreEvent>> live = liveScoreStreamer.getLive();
+
+            // then
+            StepVerifier.withVirtualTime(() -> live, () -> virtualTimeScheduler, Long.MAX_VALUE)
+                    .expectSubscription()
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(serverSentEvent -> this.assertIsLiveEvent(serverSentEvent, liveScore))
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(serverSentEvent -> this.assertIsLiveEvent(serverSentEvent, anotherLiveScore))
+                    .thenCancel().verify();
+        }
+
+        @Test
+        void return_only_keep_alive_events_after_the_first_time_event_when_time_does_not_change() {
+            // given
+            LiveScore liveScore = newInstance(LiveScore.class);
+            when(getLiveScore.execute()).thenReturn(liveScore);
+
+            // when
+            Flux<ServerSentEvent<LiveScoreEvent>> parisTimeClock = liveScoreStreamer.getLive();
+
+            // then
+            StepVerifier.withVirtualTime(() -> parisTimeClock, () -> virtualTimeScheduler, Long.MAX_VALUE)
+                    .expectSubscription()
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(serverSentEvent -> this.assertIsLiveEvent(serverSentEvent, liveScore))
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(this::assertIsKeepAliveEvent)
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(this::assertIsKeepAliveEvent)
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(this::assertIsKeepAliveEvent)
+                    .thenCancel().verify();
+        }
+
+        @Test
+        void return_keep_alive_events_between_two_time_changes() {
+            // given
+            LiveScore liveScore = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(liveScore, "score", 1);
+            LiveScore sameLiveScore = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(sameLiveScore, "score", 1);
+            LiveScore sameLiveScoreAgain = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(sameLiveScoreAgain, "score", 1);
+            LiveScore anotherLiveScore = newInstance(LiveScore.class);
+            ReflectionTestUtils.setField(anotherLiveScore, "score", 2);
+            when(getLiveScore.execute())
+                    .thenReturn(liveScore)
+                    .thenReturn(sameLiveScore)
+                    .thenReturn(sameLiveScoreAgain)
+                    .thenReturn(anotherLiveScore);
+
+            // when
+            Flux<ServerSentEvent<LiveScoreEvent>> parisTimeClock = liveScoreStreamer.getLive();
+
+            // then
+            StepVerifier.withVirtualTime(() -> parisTimeClock, () -> virtualTimeScheduler, Long.MAX_VALUE)
+                    .expectSubscription()
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(serverSentEvent -> this.assertIsLiveEvent(serverSentEvent, liveScore))
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(this::assertIsKeepAliveEvent)
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(this::assertIsKeepAliveEvent)
+                    .thenAwait(ONE_SECOND)
+                    .assertNext(serverSentEvent -> this.assertIsLiveEvent(serverSentEvent, anotherLiveScore))
+                    .thenCancel().verify();
+        }
+
         private void assertIsLiveEvent(ServerSentEvent<LiveScoreEvent> serverSentEvent, LiveScore liveScore) {
             assertThat(serverSentEvent.data().getLiveScore()).isEqualTo(liveScore);
             assertThat(serverSentEvent.data().getEventTimestamp()).isEqualTo(1506511512000L);
             assertThat(serverSentEvent.event()).isEqualTo("live-score");
             assertThat(serverSentEvent.comment()).isNull();
+        }
+
+        private void assertIsKeepAliveEvent(ServerSentEvent serverSentEvent) {
+            assertThat(serverSentEvent.data()).isNull();
+            assertThat(serverSentEvent.event()).isEqualTo("live-score");
+            assertThat(serverSentEvent.comment()).isEqualTo("keep-alive");
         }
     }
 }
